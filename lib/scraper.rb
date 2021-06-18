c796b417096fefa3a989b436e6d168ab43be1d8b
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:45:10 -0400

    Archive Flatiron code.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
+require 'awesome_print'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+# courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+p doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].children[0].attributes
+
+# courses.each do |course|
+#     puts course.text.strip
+# end
